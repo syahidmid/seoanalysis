529ac15f145de27cc403a8c6b8d0600b0784a006
syahidmid <syahidmuhammad19@gmail.com>
2023-06-03 21:12:06 +0700

    Make sure program stop when URL not valid

--- 1_🤠SEO_Content_Analysis.py
+++ 1_🤠SEO_Content_Analysis.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import time
+import sys
 from scrape import (
     get_title,
     get_description,
@@ -41,112 +42,111 @@ if not url.startswith("http"):
 if st.button("Analyze"):
     if not url or not primary_keyword:
         st.warning("Please enter both URL and primary keyword")
+    elif not is_valid_url(url):
+        st.warning("Please enter a valid URL")
     else:
-        if not is_valid_url(url):
-            st.warning("Please enter a valid URL")
-        else:
-            if get_status_code(url) != 200:
-                st.warning("The website is not up and running")
-            else:
-                with st.spinner("Start scraping the website..."):
-                    time.sleep(1)
-                    seo_title = get_title(url)
-                    h1 = get_h1(url)
-                    st.header(seo_title)
-                    description = get_description(url)
-                    st.write(description)
+        status_code = get_status_code(url)
+        if get_status_code(url) != 200:
+            st.warning("The website is not up and running")
+            sys.exit(
+                1
+            )  # Exit the program with an exit code of 1 # Hentikan eksekusi program di sini
+        with st.spinner("Start scraping the website..."):
+            time.sleep(1)
+            seo_title = get_title(url)
+            # Lanjutkan eksekusi program di sini
+            h1 = get_h1(url)
+            st.header(seo_title)
+            description = get_description(url)
+            st.write(description)
 
-                with st.spinner("Trying to identify the content area..."):
-                    time.sleep(1)
-                    disclaimer_message = domain_disclaimer(url)
-                    st.subheader(":blue[Overview]")
-                    st.markdown(disclaimer_message)
+            with st.spinner("Trying to identify the content area..."):
+                time.sleep(1)
+                disclaimer_message = domain_disclaimer(url)
+                st.subheader(":blue[Overview]")
+                st.markdown(disclaimer_message)
 
-                with st.spinner("Starting content analysis..."):
-                    time.sleep(1)
-                    content_html = get_content_with_html(url)
-                    content = get_content(url)
-                    headings = get_headings(content_html)
-                    title_length = count_title_length(seo_title)
+            with st.spinner("Starting content analysis..."):
+                time.sleep(1)
+                content_html = get_content_with_html(url)
+                content = get_content(url)
+                headings = get_headings(content_html)
+                title_length = count_title_length(seo_title)
 
-                with st.spinner("Calculating word count..."):
-                    time.sleep(1)
-                    word_count = count_words(content)
-                    meta_description_length = count_meta_description(description)
-                with st.spinner("Comparing SEO title with H1..."):
-                    seo_title_h1_result = compare_seo_title_h1(seo_title, h1)
-                    keyword_in_h1_result = check_primary_keyword_in_h1(
-                        primary_keyword, h1
-                    )
-                with st.spinner("Calculating keyword density..."):
-                    time.sleep(1)
-                    keyword_density = check_primary_keyword_in_content(
-                        primary_keyword, content
-                    )
-                    table_data = {
-                        "Item": [
-                            "Title Length",
-                            "Meta Description Length",
-                            "Content Length",
-                            "SEO Title Compatibility with H1",
-                            "Primary Keyword in H1",
-                            "Keyword density",
-                        ],
-                        "Result": [
-                            title_length,
-                            meta_description_length,
-                            word_count,
-                            seo_title_h1_result,
-                            keyword_in_h1_result,
-                            keyword_density,
-                        ],
-                    }
-                    st.write("\n\n")
-                    st.table(table_data)
-                with st.spinner("Checking related keywords..."):
-                    time.sleep(1)
-                    related_keywords_result = check_related_keywords(
-                        content_html, related_keywords_list
-                    )
-                    outline_expander = st.expander("Outline Analysis")
-                    related_keywords_expander = st.expander(
-                        "Related Keywords", expanded=True
-                    )
-                    internal_links_expander = st.expander(
-                        "Internal Links", expanded=True
-                    )
-
-                # Outline Analysis
-            with st.expander("Outline", expanded=True):
-                tab1, tab2, tab3, tab4, tab5 = st.tabs(
-                    [
-                        "My Article",
-                        "Competitor 1",
-                        "Competitor 2",
-                        "Competitor 3",
-                        "Competitor 4",
-                    ]
+            with st.spinner("Calculating word count..."):
+                time.sleep(1)
+                word_count = count_words(content)
+                meta_description_length = count_meta_description(description)
+            with st.spinner("Comparing SEO title with H1..."):
+                seo_title_h1_result = compare_seo_title_h1(seo_title, h1)
+                keyword_in_h1_result = check_primary_keyword_in_h1(primary_keyword, h1)
+            with st.spinner("Calculating keyword density..."):
+                time.sleep(1)
+                keyword_density = check_primary_keyword_in_content(
+                    primary_keyword, content
                 )
-                with tab1:
-                    st.subheader(":blue[Headings]")
-                    headings = get_all_headings(url)
-                    for heading in headings:
-                        st.write(heading)
-                # Part of the Streamlit main function
-                with tab2:
-                    st.write("Hello")
-                with tab3:
-                    st.write("Hello")
-                with tab4:
-                    st.write("Hello")
-                with tab5:
-                    st.write("Hello")
-            # Related Keywords Analysis
-            with st.expander("Related Keywords"):
-                st.header(":blue[Related keywords]")
-                st.table(related_keywords_result)
-            # Internal Links Analysis
-            with st.expander("Internal Links"):
-                st.subheader(":blue[Internal Links Analysis]")
-                internal_links_table = get_internal_links(url)
-                st.table(internal_links_table)
+                related_keywords_result = check_related_keywords(
+                    content_html, related_keywords_list
+                )
+
+                table_data = {
+                    "Item": [
+                        "Title Length",
+                        "Meta Description Length",
+                        "Content Length",
+                        "SEO Title Compatibility with H1",
+                        "Primary Keyword in H1",
+                        "Keyword density",
+                    ],
+                    "Result": [
+                        title_length,
+                        meta_description_length,
+                        word_count,
+                        seo_title_h1_result,
+                        keyword_in_h1_result,
+                        keyword_density,
+                    ],
+                }
+                st.write("\n\n")
+                st.table(table_data)
+                outline_expander = st.expander("Outline Analysis")
+                related_keywords_expander = st.expander(
+                    "Related Keywords", expanded=True
+                )
+                internal_links_expander = st.expander("Internal Links", expanded=True)
+
+            # Outline Analysis
+        with st.expander("Outline", expanded=True):
+            tab1, tab2, tab3, tab4, tab5 = st.tabs(
+                [
+                    "My Article",
+                    "Competitor 1",
+                    "Competitor 2",
+                    "Competitor 3",
+                    "Competitor 4",
+                ]
+            )
+            with tab1:
+                st.subheader(":blue[Headings]")
+                headings = get_all_headings(url)
+                for heading in headings:
+                    st.write(heading)
+            # Part of the Streamlit main function
+            with tab2:
+                st.write("Hello")
+            with tab3:
+                st.write("Hello")
+            with tab4:
+                st.write("Hello")
+            with tab5:
+                st.write("Hello")
+        # Related Keywords Analysis
+        with st.expander("Related Keywords"):
+            st.header(":blue[Related keywords]")
+
+            st.table(related_keywords_result)
+        # Internal Links Analysis
+        with st.expander("Internal Links"):
+            st.subheader(":blue[Internal Links Analysis]")
+            internal_links_table = get_internal_links(url)
+            st.table(internal_links_table)
--- keywords.py
+++ keywords.py
@@ -5,24 +5,32 @@ from scrape import get_h1
 import re
 
 
-
 def check_related_keywords(content_html, related_keywords_list):
-    soup = BeautifulSoup(content_html, 'html.parser')
+    soup = BeautifulSoup(content_html, "html.parser")
     content = soup.text.lower()
     content_length = len(content)
     related_keywords_result = []
-    for keyword in related_keywords_list:
-        keyword = keyword.lower()
-        keyword_count = content.count(keyword)
-        if keyword_count > 0:
-            found = True
-            percentage = round((keyword_count / content_length) * 100, 2)
-            how_many = f"{keyword_count} ({percentage}%)"
-        else:
-            found = False
-            how_many = "0 (0%)"
-        related_keywords_result.append([keyword, found, how_many])
-    return DataFrame(related_keywords_result, columns=['Related Keywords', 'Found in Article', 'How Many Times'])
+
+    if content_length > 0:
+        for keyword in related_keywords_list:
+            keyword = keyword.lower()
+            keyword_count = content.count(keyword)
+            if keyword_count > 0:
+                found = True
+                percentage = round((keyword_count / content_length) * 100, 2)
+                how_many = f"{keyword_count} ({percentage}%)"
+            else:
+                found = False
+                how_many = "0 (0%)"
+            related_keywords_result.append([keyword, found, how_many])
+    else:
+        related_keywords_result.append(["No content available", False, "0 (0%)"])
+
+    return DataFrame(
+        related_keywords_result,
+        columns=["Related Keywords", "Found in Article", "How Many Times"],
+    )
+
 
 def compare_seo_title_h1(seo_title, h1):
     """Compare the SEO title with the H1"""
@@ -39,6 +47,7 @@ def check_primary_keyword_in_h1(primary_keyword, h1):
     else:
         return False
 
+
 def check_primary_keyword_in_content(primary_keyword, content):
     # Convert both primary keyword and content to lower case to ensure case-insensitive checking
     primary_keyword = primary_keyword.lower()
@@ -52,7 +61,6 @@ def check_primary_keyword_in_content(primary_keyword, content):
 
     # If the keyword count is greater than zero, the keyword is present in the content
     if keyword_count > 0:
-        return [ f"True, {keyword_count} times ({keyword_density}%)"]
+        return [f"True, {keyword_count} times ({keyword_density}%)"]
     else:
         return [primary_keyword, "False, 0 times (0%)"]
-
