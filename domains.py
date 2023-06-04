b988053804c1e7609ff47de6a5faaeca82437089
syahidmid <syahidmuhammad19@gmail.com>
2023-06-04 18:14:23 +0700

    Update content area

--- domains.py
+++ domains.py
@@ -1,8 +1,15 @@
 CONTENT_AREA = {
     "auto2000.co.id": "tips-detail-content",
+    "ahrefs": "post-content",
+    "allianz": "text",
+    "carmudi": "single-entry-summary-post-content",
+    "duitpintar": "blog-post_content",
+    "doktermobil": "elementor-widget-container",
+    "gridoto": "read__article clearfix",
+    "idntimes": "content-post-description full",
+    "kompas": "read__content",
     "lifepal": "SinglePost_post__content__3RMzn",
+    "mobbi": "article-details-content",
     "otoklix": "post-content",
     "qoala": "post-content",
-    "duitpintar": "blog-post_content",
-    "ahrefs": "post-content",
 }
