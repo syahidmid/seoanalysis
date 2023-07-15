74ee743d6490e6eb39d00da96558cf8b0d36f54f
syahidmid <syahidmuhammad19@gmail.com>
2023-07-15 16:39:12 +0700

    add

--- pages/3_🤠Prompt_Generators.py
+++ pages/3_🤠Prompt_Generators.py
@@ -17,6 +17,8 @@ class PromptGenerator:
             self.session_state['primary_keyword'] = ''
         if 'outline' not in self.session_state:
             self.session_state['outline'] = ''
+        if 'references' not in self.session_state:
+            self.session_state['references'] = ''
 
     def generate_prompt(self):
         topic = st.text_input("Topic:", value=self.session_state['topic'], key=f"{self.prompt_name}_topic")
@@ -25,9 +27,10 @@ class PromptGenerator:
         reader_greeting = st.selectbox("Sapaan ke pembaca:", options=["Kamu", "Anda"], index=0 if self.session_state['reader_greeting'] == 'Kamu' else 1, key=f"{self.prompt_name}_reader_greeting")
         primary_keyword = st.text_input("Primary keyword:", value=self.session_state['primary_keyword'], key=f"{self.prompt_name}_primary_keyword")
         outline = st.text_area("Outline:", value=self.session_state['outline'], key=f"{self.prompt_name}_outline")
+        references = st.text_area('References: ', value=self.session_state['references'], key=f"{self.prompt_name}_references")
 
         if st.button("Generate"):
-            prompt = self.generate_prompt_text(topic, length, style, reader_greeting, primary_keyword, outline)
+            prompt = self.generate_prompt_text(topic, length, style, reader_greeting, primary_keyword, outline, references)
             st.session_state[f"{self.prompt_name}_result"] = prompt
 
         if f"{self.prompt_name}_result" in st.session_state:
@@ -41,13 +44,17 @@ class PromptGenerator:
         self.session_state['reader_greeting'] = reader_greeting
         self.session_state['primary_keyword'] = primary_keyword
         self.session_state['outline'] = outline
+        self.session_state['references'] = references
 
-    def generate_prompt_text(self, topic, length, style, reader_greeting, primary_keyword, outline):
+    def generate_prompt_text(self, topic, length, style, reader_greeting, primary_keyword, outline, references):
         prompt = f"Tolong buatkan artikel tentang {topic} dengan primary keyword {primary_keyword} sepanjang {length} kata."
-        prompt += f"Gunakan gaya bahasa {style} dan {reader_greeting} untuk menyebut pembaca. Perhatikan penempatan kata kunci, wajib disebut di awal kalimat dan body artikel."
+        prompt += f"Gunakan gaya bahasa {style} dengan kata '{reader_greeting}' untuk menyebut pembaca. Perhatikan penempatan kata kunci, wajib disebut di awal kalimat dan body artikel."
+        prompt += f"Gunakan referensi di bawah ini sebagai sumber pembuatan artikel tetapi jangan sampai duplikat. Kamu bisa ubah pilihan kata dan struktur kalimat dan tambahkan informasi relevan yang valid."
         prompt += f"\nPrimary keyword: {primary_keyword}."
         prompt += f"\nOutline: {outline}."
+        prompt += f"\nReferences: {references}."
         return prompt
+
 
 def main():
     prompt_generator_1 = PromptGenerator(prompt_name="Outline Based Writing")
