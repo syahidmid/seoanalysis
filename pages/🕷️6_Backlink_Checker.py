c26971da9617d1940fba5669a9b0a25fe38489c7
Syahid Muhammad <69981696+syahidmid@users.noreply.github.com>
2024-03-20 03:44:19 +0000

    clean up backlink checker

--- pages/🕷️6_Backlink_Checker.py
+++ pages/🕷️6_Backlink_Checker.py
@@ -24,54 +24,25 @@ from analyzers.count import (
     word_counter,
     character_counter,
 )
-# Initialize session state
+
 if 'seo_results_df' not in st.session_state:
     st.session_state['seo_results_df'] = None
 
-# Judul aplikasi
 st.title("🕷️Backlink Checker")
-# Pengaturan
-with st.expander("Pengaturan"):
-    show_title_length = st.checkbox("Tampilkan Panjang Judul")
-
-# Pilihan input URL
-input_option = st.selectbox("Pilih metode input URL:", ["Text Area", "Upload File CSV", "URL dan Kata Kunci"])
+input_option = st.selectbox("Pilih metode input URL:", ["Text Area", "Upload File CSV"])
 if input_option == "Text Area":
-    # Input URL dari pengguna melalui text area
     urls = st.text_area("Masukkan URL (pisahkan dengan Enter)", height=200)
     urls = urls.split('\n')
-    primary_keyword = ""  # Kosongkan primary_keyword
+    primary_keyword = "" 
 elif input_option == "Upload File CSV":
-    # Unggah file CSV
     uploaded_file = st.file_uploader("Unggah file CSV", type=["csv"])
     if uploaded_file is not None:
         urls_df = pd.read_csv(uploaded_file)
-        # Periksa apakah kolom "URL" ada dalam DataFrame
         if "URL" in urls_df.columns:
             urls = urls_df["URL"].tolist()
-            primary_keyword = ""  # Kosongkan primary_keyword
+            primary_keyword = "" 
         else:
             st.warning("File CSV tidak memiliki kolom 'URL'.")
-            st.stop()
-    else:
-        st.warning("Silakan unggah file CSV terlebih dahulu.")
-        st.stop()
-else:
-    # Input URL dan Kata Kunci dari pengguna
-    primary_keyword = ""
-    uploaded_file = st.file_uploader("Unggah file CSV (harus berisi dua kolom: URL, Primary Keyword)", type=["csv"])
-    if uploaded_file is not None:
-        urls_df = pd.read_csv(uploaded_file)
-        
-        # Simpan nama kolom dalam bentuk array
-        column_names = urls_df.columns.tolist()
-
-        # Periksa apakah kolom "URL" dan "Primary Keyword" ada dalam DataFrame
-        if "URL" in column_names and "Primary Keyword" in column_names:
-            urls = urls_df["URL"].tolist()
-            primary_keywords = urls_df["Primary Keyword"].tolist()
-        else:
-            st.warning("File CSV harus memiliki dua kolom: URL dan Primary Keyword.")
             st.stop()
     else:
         st.warning("Silakan unggah file CSV terlebih dahulu.")
@@ -123,17 +94,12 @@ if st.button("Scrape dan Analisis"):
             data_content_r['Backlinks to Lifepal'] = backlinks_lifepal
         result_content.append(data_content_r)
 
-        # Update progress bar
+    
         progress_percent = min((index + 1) / total_urls, 1.0) if total_urls != 0 else 1.0  # Normalisasi nilai progress_percent
         progress_bar.progress(progress_percent, text=f"Progress: {index+1}/{total_urls} URLs scraped")
-        time.sleep(0.1)  # Untuk memberikan sedikit jeda agar progress bar dapat dilihat
+        time.sleep(0.1)
 
-    # Hapus progress bar setelah selesai
     progress_bar.empty()
     df_content = pd.DataFrame(result_content)
-
-    # Menyimpan DataFrame ke dalam session state
     st.session_state.seo_df_content = df_content
-
-    # Mencetak DataFrame ke dalam antarmuka pengguna
     st.dataframe(st.session_state.seo_df_content)
